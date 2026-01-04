802a5199e878c315d71cb6ffbd58b2f993004174
mattgregory-dev <mattgregory-dev@users.noreply.github.com>
2026-01-04 08:12:22 -0700

    chrome: auth clean up

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -54,7 +54,30 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true
+
+  config.action_mailer.delivery_method = :smtp
+
+  config.action_mailer.smtp_settings = {
+    user_name: "apikey",
+    password: ENV.fetch("SENDGRID_API_KEY"),
+    domain: ENV.fetch("APP_HOST"),
+    address: "smtp.sendgrid.net",
+    port: 2525,
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
+
+  config.action_mailer.default_url_options = {
+    host: ENV.fetch("APP_HOST"),
+    protocol: "https"
+  }
+
+  config.action_mailer.default_options = {
+    from: ENV.fetch("MAILER_FROM")
+  }
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
